b5fe48641ee9e170e1e2da3b05cd95d52f248222
Riley Kuhlman <rilstarssun@gmail.com>
2022-09-26 15:24:47 -0500

    Update about text

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,12 +8,11 @@ class About extends Component {
       <div>
         <Title>About</Title>
         <div className="about-text">
-          Hi, I'm Riley Kuhlman. I've been programming off and on ever since I
-          was 14 years old. I started learning on Khan Academy, and once I
-          finished the course there I moved to YouTube courses. I really enjoyed
-          following along with Daniel Shiffman's tutorials. I've dabbled in lots
-          of different languages, but the ones I always come back to are
-          Javascript and Python.
+          Hi, I'm Riley Kuhlman. I've been programming off and on since I was 14 years old. I started learning on Khan Academy and YouTube (I really enjoyed following along with Daniel Shiffman's tutorials), then took a few classes in high school. I continued learning by joining a few clubs, finishing fourth at State in 2021 with the CS UIL team and writing a Python simulation for my rocketry team.
+
+          I currently attend Rice University, where I am majoring in computer science. I am a member of Rice Eclipse, where I have CAD-ed most of the test stand for the Proxima hybrid engine and am working towards my L1 certification. I run on the varsity cross country team, and I enjoy grinding on LeetCode before I get my morning run in.
+
+          I've dabbled in many languages, but I typically come back to Javascript and Python. Javascript is ubiquitous, and I love making websites that other people can actually see and use. I enjoy writing code in Python, as I find it the easiest way to transform my ideas into code. If I ever need to write anything fast, I am proficient in Java (although it isn't my favorite), and I have a bit of experience solving algorithms in C.
         </div>
       </div>
     );
