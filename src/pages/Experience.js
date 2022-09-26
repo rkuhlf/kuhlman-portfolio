a48eedb2d0934647377a3cd9af1f71c5cea941bc
Riley Kuhlman <rilstarssun@gmail.com>
2022-09-26 15:35:12 -0500

    Updated my experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -4,15 +4,19 @@ import Title from "../helpers/Title";
 
 class Experience extends Component {
   render() {
+    const years_programming = new Date().getFullYear() - 2017;
+
     return (
       <div>
         <Title>Experience</Title>
         <div className="about-text">
-          I have volunteered for two summers at the local library in Lake
-          Jackson, working to help shelve books and help people find what they
-          need. I have also volunteered for the highschool soccer team as a
-          ballboy and to work the concession stand. Unfortunately, I have no
-          paid job experience yet, but I'm eager to start.
+          I have {years_programming} years of experience programming, split pretty evenly between front-end development and numerical / algorithm-based projects. I have some experience with data science - I developed an analysis of NBA salaries and a soccer goals predictor - and I am familiar with basic machine learning concepts.
+
+          <br />
+          <br />
+
+          I have a decent chunk of experience volunteering. I spent two summers at the local library in Lake
+          Jackson, working to help shelve books and help people find what they need. I have also volunteered for the high school soccer team as a ballboy, as well as working the concession stand. I was parliamentarian of the Spanish Honor Society (working to organize volunteer activities), and I was a math tutor for my last two years of high school.
         </div>
       </div>
     );
